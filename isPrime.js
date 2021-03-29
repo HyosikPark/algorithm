e2dcfb1d922f33f845af1ea6f0d1cda547c5be71
hyogooz <hyosik.dev@gmail.com>
2021-03-30 03:10:27 +0900

    rm cached

--- isPrime.js
+++ isPrime.js
@@ -2,17 +2,17 @@
   const isPrime = Array.from({ length: n + 1 }, () => 1);
   isPrime[0] = isPrime[1] = 0;
 
-  for (let i = 2; i < Math.sqrt(n); i++) {
-    if (isPrime) {
+  for (let i = 0; i < Math.sqrt(n); i++) {
+    if (isPrime[i]) {
       for (let j = i ** 2; j < isPrime.length; j += i) {
-        isPrime[j] = 0;
+        if (isPrime[j]) isPrime[j] = 0;
       }
     }
   }
 
-  const answer = isPrime.reduce((acc, e, i) => {
+  const prime = isPrime.reduce((acc, e, i) => {
     return e ? acc.concat(i) : acc;
   }, []);
 
-  console.log(answer);
+  console.log(prime);
 })(100);
